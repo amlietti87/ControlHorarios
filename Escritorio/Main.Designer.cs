8c22fd0d139bdd5607ceae46bfe925bb9df0ee78
Andres Lietti <amlietti@gmail.com>
2019-01-18 15:00:50 -0300

    Se agrega pantalla principal.

---
+++ Escritorio/Main.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Escritorio
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pacientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.obrasSocialesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tiposDeHorasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sesionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarYCobrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cobradasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PanelContenedor = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pacientesToolStripMenuItem,
+            this.obrasSocialesToolStripMenuItem,
+            this.tiposDeHorasToolStripMenuItem,
+            this.sesionesToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pacientesToolStripMenuItem
+            // 
+            this.pacientesToolStripMenuItem.Name = "pacientesToolStripMenuItem";
+            this.pacientesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.pacientesToolStripMenuItem.Text = "Pacientes";
+            this.pacientesToolStripMenuItem.Click += new System.EventHandler(this.pacientesToolStripMenuItem_Click);
+            // 
+            // obrasSocialesToolStripMenuItem
+            // 
+            this.obrasSocialesToolStripMenuItem.Name = "obrasSocialesToolStripMenuItem";
+            this.obrasSocialesToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.obrasSocialesToolStripMenuItem.Text = "Obras Sociales";
+            this.obrasSocialesToolStripMenuItem.Click += new System.EventHandler(this.obrasSocialesToolStripMenuItem_Click);
+            // 
+            // tiposDeHorasToolStripMenuItem
+            // 
+            this.tiposDeHorasToolStripMenuItem.Name = "tiposDeHorasToolStripMenuItem";
+            this.tiposDeHorasToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.tiposDeHorasToolStripMenuItem.Text = "Tipos de Horas";
+            this.tiposDeHorasToolStripMenuItem.Click += new System.EventHandler(this.tiposDeHorasToolStripMenuItem_Click);
+            // 
+            // sesionesToolStripMenuItem
+            // 
+            this.sesionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarYCobrarToolStripMenuItem,
+            this.cobradasToolStripMenuItem});
+            this.sesionesToolStripMenuItem.Name = "sesionesToolStripMenuItem";
+            this.sesionesToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.sesionesToolStripMenuItem.Text = "Sesiones";
+            // 
+            // cargarYCobrarToolStripMenuItem
+            // 
+            this.cargarYCobrarToolStripMenuItem.Name = "cargarYCobrarToolStripMenuItem";
+            this.cargarYCobrarToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.cargarYCobrarToolStripMenuItem.Text = "Cargar y Cobrar";
+            // 
+            // cobradasToolStripMenuItem
+            // 
+            this.cobradasToolStripMenuItem.Name = "cobradasToolStripMenuItem";
+            this.cobradasToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.cobradasToolStripMenuItem.Text = "Cobradas";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // PanelContenedor
+            // 
+            this.PanelContenedor.Location = new System.Drawing.Point(13, 28);
+            this.PanelContenedor.Name = "PanelContenedor";
+            this.PanelContenedor.Size = new System.Drawing.Size(779, 417);
+            this.PanelContenedor.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PanelContenedor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pacientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem obrasSocialesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tiposDeHorasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sesionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarYCobrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cobradasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Panel PanelContenedor;
+    }
+}
---
+++ Escritorio/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escritorio
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFormInPanel(Form fh)
+        {
+            if (this.PanelContenedor.Controls.Count > 0)
+                this.PanelContenedor.Controls.RemoveAt(0);
+            fh.TopLevel = false;
+            fh.FormBorderStyle = FormBorderStyle.None;
+            fh.Dock = DockStyle.Fill;
+            this.PanelContenedor.Controls.Add(fh);
+            this.PanelContenedor.Tag = fh;
+            fh.Show();
+        }
+
+        private void pacientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Paciente paciente = Application.OpenForms.OfType<Paciente>().FirstOrDefault() ?? new Paciente();
+            AddFormInPanel(paciente);
+        }
+
+        private void obrasSocialesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ObraSocial obraSocial = Application.OpenForms.OfType<ObraSocial>().FirstOrDefault() ?? new ObraSocial();
+            AddFormInPanel(obraSocial);
+        }
+
+        private void tiposDeHorasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TipodeHora tipodeHora = Application.OpenForms.OfType<TipodeHora>().FirstOrDefault() ?? new TipodeHora();
+            AddFormInPanel(tipodeHora);
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
